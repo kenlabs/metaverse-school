1af39d410ababc2173f74466635b18fc2c7c04ad
wyang14 <weili.yang@verizonmedia.com>
2021-12-07 17:59:10 +0000

    separated sidebar configuration

---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,73 @@
+// .vuepress/sidebar.js
+
+module.exports = {
+    sidebar: [
+        {
+            title: 'Prequel',
+            collapsable: false,
+            children: [
+                '/Prequel/'
+            ]
+        },
+        {
+            title: 'Definition',
+            collapsable: false,
+            children: [
+                '/Definition/',
+                '/Definition/OutlierVenture/',
+                '/Definition/MatthewBall.vc/'
+            ]
+        },
+        {
+            title: 'Framework',
+            collapsable: false,
+            children:
+                [
+                    '/Framework/framework for the metaverse',
+                    '/Framework/hardware',
+                    '/Framework/networking',
+                    '/Framework/compute',
+                    '/Framework/virtual platforms',
+                    '/Framework/interchange tools and standards',
+                    '/Framework/payments, payment rails and blockchains',
+                    '/Framework/content, services and asset businesses',
+                    '/Framework/evolving user and business behaviors'
+                ]
+        },
+        {
+            title: 'Buidl',
+            collapsable: false,
+            children: [
+                '/Buidl/',
+                '/Buidl/Open Metaverse OS/',
+                '/Buidl/Open Metaverse OS/Hardware Infrastructure/',
+                '/Buidl/Open Metaverse OS/Software Backend/',
+                '/Buidl/Open Metaverse OS/Software Backend/IPFS/',
+                '/Buidl/Open Metaverse OS/Software Backend/Filecoin/',
+                '/Buidl/Open Metaverse OS/Software Backend/Connecting the Metaverse with IPFS.md',
+                '/Buidl/Open Metaverse OS/World Primitives & Rules/',
+                '/Buidl/Open Metaverse OS/In-world Assets/',
+                '/Buidl/Open Metaverse OS/End-user Hardware/',
+                '/Buidl/Open Metaverse OS/End-user Software/',
+                '/Buidl/Open Metaverse OS/Bridges/',
+                '/Buidl/Web3 Toolbox/',
+                '/Buidl/Web3 Toolbox/Peer-to-Peer-Networks/',
+                '/Buidl/Web3 Toolbox/Transaction Layer/',
+                '/Buidl/Web3 Toolbox/Programmability Layer/',
+                '/Buidl/Web3 Toolbox/Decentralised Finance/',
+                '/Buidl/Web3 Toolbox/NFTs - Sovereign Virtual Goods/',
+                '/Buidl/Web3 Toolbox/Decentralised Governance/',
+                '/Buidl/Web3 Toolbox/Decentralised Cloud Services/',
+                '/Buidl/Web3 Toolbox/Self-sovereign Identity/',
+                '/Buidl/Web3 Toolbox/Agents/'
+            ]
+        },
+        {
+            title: 'Resource',
+            collapsable: false,
+            children: [
+                '/Resource/'
+            ]
+        }
+    ]
+}
