9e4dab07b56e426392f3ea4ec1bef68cc47990f5
wyang14 <weili.yang@verizonmedia.com>
2021-12-09 21:04:59 +0000

    social share buttons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,16 @@ const SPEEDCURVE_ID = process.env.SPEEDCURVE_ID || ''
 const COUNTLY_KEY = process.env.COUNTLY_KEY || ''
 const pageSuffix = '/'
 
+const extendsNetworks = {
+  linkedin: {
+    sharer:
+      'https://www.linkedin.com/shareArticle?mini=true&url=@url&title=@title&summary=@description',
+    type: 'popup',
+    color: '#1786b1',
+    icon:
+      '<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path d="M910.336 0H113.664A114.005333 114.005333 0 0 0 0 113.664v796.672A114.005333 114.005333 0 0 0 113.664 1024h796.672A114.005333 114.005333 0 0 0 1024 910.336V113.664A114.005333 114.005333 0 0 0 910.336 0zM352.256 796.330667H207.189333V375.466667h145.066667z m-72.021333-477.866667a77.824 77.824 0 0 1-81.237334-74.069333A77.824 77.824 0 0 1 280.234667 170.666667a77.824 77.824 0 0 1 81.237333 73.728 77.824 77.824 0 0 1-81.237333 73.386666z m582.314666 477.866667H716.8v-227.669334c0-46.762667-18.432-93.525333-73.045333-93.525333a84.992 84.992 0 0 0-81.237334 94.549333v226.304h-140.629333V375.466667h141.653333v60.757333a155.989333 155.989333 0 0 1 136.533334-71.338667c60.416 0 163.498667 30.378667 163.498666 194.901334z" /></svg>',
+  },
+}
 module.exports = {
   base: '/',
   head: require('./head'),
@@ -25,8 +35,8 @@ module.exports = {
       md.use(require('markdown-it-footnote'))
       md.use(require('markdown-it-task-lists'))
       md.use(require('markdown-it-deflist')),
-      md.use(require('markdown-it-imsize')),
-      md.use(require('markdown-it-image-lazy-loading'))
+        md.use(require('markdown-it-imsize')),
+        md.use(require('markdown-it-image-lazy-loading'))
     }
   },
   themeConfig: {
@@ -84,18 +94,18 @@ module.exports = {
           {
             title: 'Framework',
             collapsable: false,
-            children: 
-            [
-              '/framework/framework-for-the-metaverse',
-              '/framework/hardware',
-              '/framework/networking',
-              '/framework/compute',
-              '/framework/virtual-platforms',
-              '/framework/interchange-tools-and-standards',
-              '/framework/payments-payment-rails-and-blockchains',
-              '/framework/content-services-and-asset-businesses',
-              '/framework/evolving-user-and-business-behaviors'
-            ]
+            children:
+              [
+                '/framework/framework-for-the-metaverse',
+                '/framework/hardware',
+                '/framework/networking',
+                '/framework/compute',
+                '/framework/virtual-platforms',
+                '/framework/interchange-tools-and-standards',
+                '/framework/payments-payment-rails-and-blockchains',
+                '/framework/content-services-and-asset-businesses',
+                '/framework/evolving-user-and-business-behaviors'
+              ]
           },
           {
             title: 'Buidl',
@@ -103,37 +113,37 @@ module.exports = {
             children: [
               ['/buidl/', 'Introduction'],
               {
-                  title: 'Open Metaverse OS',
-                  collapsable: false,
-                  children: [
-                    '/buidl/open-metaverse-os/',
-                    '/buidl/open-metaverse-os/hardware-infrastructure/',
-                    '/buidl/open-metaverse-os/software-backend/',
-                    '/buidl/open-metaverse-os/software-backend/ipfs/',
-                    '/buidl/open-metaverse-os/software-backend/filecoin/',
-                    '/buidl/open-metaverse-os/software-backend/connecting-the-metaverse-with-ipfs.md',
-                    '/buidl/open-metaverse-os/world-primitives-and-rules/',
-                    '/buidl/open-metaverse-os/in-world-assets/',
-                    '/buidl/open-metaverse-os/end-user-hardware/',
-                    '/buidl/open-metaverse-os/end-user-software/',
-                    '/buidl/open-metaverse-os/bridges/'
-                  ]
+                title: 'Open Metaverse OS',
+                collapsable: false,
+                children: [
+                  '/buidl/open-metaverse-os/',
+                  '/buidl/open-metaverse-os/hardware-infrastructure/',
+                  '/buidl/open-metaverse-os/software-backend/',
+                  '/buidl/open-metaverse-os/software-backend/ipfs/',
+                  '/buidl/open-metaverse-os/software-backend/filecoin/',
+                  '/buidl/open-metaverse-os/software-backend/connecting-the-metaverse-with-ipfs.md',
+                  '/buidl/open-metaverse-os/world-primitives-and-rules/',
+                  '/buidl/open-metaverse-os/in-world-assets/',
+                  '/buidl/open-metaverse-os/end-user-hardware/',
+                  '/buidl/open-metaverse-os/end-user-software/',
+                  '/buidl/open-metaverse-os/bridges/'
+                ]
               },
               {
-                  title: 'Web3 Toolbox',
-                  collapsable: false,
-                  children: [
-                    '/buidl/web3-toolbox/',
-                    '/buidl/web3-toolbox/peer-to-peer-networks/',
-                    '/buidl/web3-toolbox/transaction-layer/',
-                    '/buidl/web3-toolbox/programmability-layer/',
-                    '/buidl/web3-toolbox/decentralised-finance/',
-                    '/buidl/web3-toolbox/nfts-sovereign-virtual-goods/',
-                    '/buidl/web3-toolbox/decentralised-governance/',
-                    '/buidl/web3-toolbox/decentralised-cloud-services/',
-                    '/buidl/web3-toolbox/self-sovereign-identity/',
-                    '/buidl/web3-toolbox/agents/'
-                  ]
+                title: 'Web3 Toolbox',
+                collapsable: false,
+                children: [
+                  '/buidl/web3-toolbox/',
+                  '/buidl/web3-toolbox/peer-to-peer-networks/',
+                  '/buidl/web3-toolbox/transaction-layer/',
+                  '/buidl/web3-toolbox/programmability-layer/',
+                  '/buidl/web3-toolbox/decentralised-finance/',
+                  '/buidl/web3-toolbox/nfts-sovereign-virtual-goods/',
+                  '/buidl/web3-toolbox/decentralised-governance/',
+                  '/buidl/web3-toolbox/decentralised-cloud-services/',
+                  '/buidl/web3-toolbox/self-sovereign-identity/',
+                  '/buidl/web3-toolbox/agents/'
+                ]
               }
             ]
           },
@@ -149,7 +159,13 @@ module.exports = {
     },
   },
   plugins: [
-    'social-share',
+    [
+      'social-share',
+      {
+        networks: ['facebook', 'twitter', 'linkedin', 'reddit', 'weibo', 'qq', 'douban', 'whatsapp'],
+        extendsNetworks
+      }
+    ],
     [require('./plugins/vuepress-plugin-speedcurve'), { id: SPEEDCURVE_ID }],
     [require('./plugins/vuepress-plugin-countly'), {
       domain: DEPLOY_DOMAIN,
